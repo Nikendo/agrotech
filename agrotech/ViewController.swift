69ad3c4b6e6fc79006fbd160305837a43f71d3b1
Nikita <pravdomir42@gmail.com>
2019-11-12 21:45:14 +0300

    Initial Commit

---
+++ agrotech/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  agrotech
+//
+//  Created by n.shmatov on 12.11.2019.
+//  Copyright © 2019 n.shmatov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
